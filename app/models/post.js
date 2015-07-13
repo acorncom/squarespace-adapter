0b66bd59293361560f0b9fc136e9e8803ae94316
Kyle Coberly <kyle.coberly@gmail.com>
2015-07-13 01:49:48 -0600

    Fixing model

--- app/models/post.js
+++ app/models/post.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     title: DS.attr(),
-    date: DS.attr("date"),
+    datePublished: DS.attr("date"),
     heroImage: DS.attr(),
     excerpt: DS.attr(),
-    readMoreLink: DS.attr()
+    content: DS.attr()
 });
