527d5121cf75f458896ffe9db3709e7c3e57fe1d
Kyle Coberly <kyle.coberly@gmail.com>
2015-07-13 01:24:37 -0600

    Refactoring adapter and serializer

--- app/components/squarespace-post/component.js
+++ app/components/squarespace-post/component.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    cleanRawPost: Ember.on("didInsertElement", function(){
-        console.log(this.get("post"));
-        let posts  = /<article\b[^>]*>([\s\S]*?)<\/article>/g.exec(this.get("post"));
-        this.set("cleanPost", posts[0]);
-    })
 });
--- app/posts/route.js
+++ app/posts/route.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
-import config from "../config/environment";
 
 export default Ember.Route.extend({
     model: function(){
-        let url = config.APP.CORS_PROXY + "/http://www.forloveandlemons.com/blog";
-        return Ember.$.get(url).then(function(post){
-            return post;
-        });
+        return this.store.find("post", "girls-of-summer-jessica-morrow");
     },
     setupController: function(controller, model){
         controller.set("post", model);
