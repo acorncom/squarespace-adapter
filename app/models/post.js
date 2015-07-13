9e1c5339ea99592b8e264753cfe8aa6c975076c0
Kyle Coberly <kyle.coberly@gmail.com>
2015-07-12 21:32:54 -0600

    Adding blog post.

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    title: DS.attr(),
+    date: DS.attr("date"),
+    heroImage: DS.attr(),
+    excerpt: DS.attr(),
+    readMoreLink: DS.attr()
 });
