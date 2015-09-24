dd3aa2fa0ba2088b66cd0f527d30251b757b277b
David Baker <dbaker@acorncomputersolutions.com>
2015-09-24 08:15:02 -0600

    Changed where we store the readability token

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,10 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 import config from '../config/environment';
-import apiTokens from './tokens';
 
 export default DS.RESTAdapter.extend({
     proxy: config.APP.CORS_PROXY,
-    token: apiTokens().readabilityToken,
+    token: config.APP.READABILITY_TOKEN,
     blog: config.APP.BLOG,
     host: config.APP.PARSER
 });
